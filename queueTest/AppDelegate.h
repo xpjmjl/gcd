6847c3a2c7c760aa5d3144bff07cac9b162a4c5e
xupengju <xupengju@qupeiyin.com>
2018-12-12 15:07:17 +0800

    Initial Commit

---
+++ queueTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  queueTest
+//
+//  Created by xupengju on 2018/12/12.
+//  Copyright © 2018年 guohongtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
